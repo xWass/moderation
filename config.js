c31e3df7ebeb6a64a597ea45d68a66d916b4f755
xWass <73602180+xWass@users.noreply.github.com>
2023-11-12 23:01:41 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,427 @@
+/** @format */
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const chalk = require("chalk");
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("config")
+        .setDescription("Change bot configurations.")
+        .addSubcommand((sub) =>
+            sub
+                .setName("verify")
+                .setDescription("Modifies server verification.")
+                .addStringOption((option) =>
+                    option
+                        .setName("modify")
+                        .setDescription("Enable or Disable?")
+                        .addChoices(
+                            { name: "Enable", value: "Enable" },
+                            { name: "Disable", value: "Disable" }
+                        )
+                        .setRequired(true)
+                )
+                .addChannelOption((option) =>
+                    option
+                        .setName("channel")
+                        .setDescription("Channel to use for verification.")
+                        .setRequired(true)
+                )
+                .addRoleOption((option) =>
+                    option
+                        .setName("role")
+                        .setDescription(
+                            "The role that the bot gives to verified users."
+                        )
+                        .setRequired(true)
+                )
+        )
+        .addSubcommand((sub) =>
+            sub
+                .setName("logging")
+                .setDescription("Modifies logging.")
+                .addStringOption((option) =>
+                    option
+                        .setName("modify")
+                        .setDescription("Enable or Disable?")
+                        .addChoices(
+                            { name: "Enable", value: "Enable" },
+                            { name: "Disable", value: "Disable" }
+                        )
+                        .setRequired(true)
+                )
+                .addChannelOption((option) =>
+                    option
+                        .setName("channel")
+                        .setDescription("Channel to use for logging.")
+                        .setRequired(true)
+                )
+                .addStringOption((option) =>
+                    option
+                        .setName("level")
+                        .setDescription(
+                            "Set logging level. Low for moderation only, high for everything."
+                        )
+                        .addChoices(
+                            { name: "Low", value: "Low" },
+                            { name: "High", value: "High" }
+                        )
+                        .setRequired(true)
+                )
+        ),
+
+    async execute(interaction, client) {
+        const db = await client.db.collection("Infractions");
+        console.log(
+            `${chalk.greenBright(
+                "[EVENT ACKNOWLEDGED]"
+            )} interactionCreate with command config`
+        );
+        const moderator = interaction.user.tag;
+        // use modify and chan for logging
+        const modify = await interaction.options.getString("modify");
+        const chan = await interaction.options.getChannel("channel");
+        const warn = await interaction.options.getString("warn");
+        const role = await interaction.options.getRole("role");
+        // __
+        const level = await interaction.options.getString("level");
+        let value;
+        if (modify === "Enable") value = true;
+        if (modify === "Disable") value = false;
+        let enableWarn;
+        if (warn === "Enable") enableWarn = true;
+        if (warn === "Disable") enableWarn = false;
+
+        const found = await db.findOne({ "guild.id": interaction.guild.id });
+        if (
+            !interaction.member.permissions.has("MANAGE_SERVER") ||
+            !interaction.guild.members.me.permissions.has("MANAGE_SERVER")
+        ) {
+            interaction.reply({
+                embeds: [
+                    {
+                        description: "You are not a moderator!",
+                        footer: {
+                            text: "This requires the Manage Server permission.",
+                        },
+                    },
+                ],
+            });
+            return;
+        }
+
+        const chosen = await interaction.options.getSubcommand();
+        if (chosen === "verify") {
+            if (!found) {
+                await db.insertOne({
+                    guild: {
+                        id: interaction.guild.id,
+                        infractions: {},
+                        config: {
+                            automod: {
+                                status: false,
+                                warn: false,
+                            },
+                            verify: {
+                                status: value,
+                                channel: chan.id,
+                                role: role.id,
+                            },
+                            logging: {
+                                status: false,
+                                channel: null,
+                                level: null,
+                            },
+                        },
+                    },
+                });
+                try {
+                    await chan.permissionOverwrites.set([
+                        {
+                            id: role.id,
+                            allow: "VIEW_CHANNEL",
+                        },
+                        {
+                            id: interaction.guild.id,
+                            deny: "VIEW_CHANNEL",
+                        },
+                    ]);
+                } catch (err) {
+                    interaction.reply({
+                        embeds: [
+                            {
+                                title: `I can't change permissions in #${chan.name}`,
+                                description: `\`\`\`${err}\`\`\``,
+                                footer: {
+                                    text: "I require the Manage Permissions role, and for my highest role to be above the verification role",
+                                },
+                            },
+                        ],
+                    });
+                    return;
+                }
+
+                try {
+                    chan.send({
+                        embeds: [
+                            {
+                                description:
+                                    "To verify, click this and press enter: </verify:1042262928969170944>",
+                            },
+                        ],
+                    });
+                } catch (err) {
+                    interaction.reply({
+                        embeds: [
+                            {
+                                title: `I do not have access to #${chan.name}`,
+                                description: `\`\`\`${err}\`\`\``,
+                                footer: {
+                                    text: "Role permissions were successfully changed.",
+                                },
+                            },
+                        ],
+                    });
+                    return;
+                }
+
+                interaction.reply({
+                    embeds: [
+                        {
+                            description: `Verify has been set to \`${modify}d\` \nVerify Channel: <#${chan.id}> \nVerify Role: <@&${role.id}>`,
+                            footer: {
+                                text: `Moderator: ${moderator}`,
+                            },
+                            color: "GREEN",
+                        },
+                    ],
+                });
+
+                return;
+            } else {
+                if (value === true) {
+                    await db.updateOne(
+                        {
+                            "guild.id": interaction.guild.id,
+                        },
+                        {
+                            $set: {
+                                "guild.config.verify.status": value,
+                                "guild.config.verify.channel": chan.id,
+                                "guild.config.verify.role": role.id,
+                            },
+                        }
+                    );
+                    try {
+                        await chan.permissionOverwrites.set([
+                            {
+                                id: role.id,
+                                allow: "VIEW_CHANNEL",
+                            },
+                            {
+                                id: interaction.guild.id,
+                                deny: "VIEW_CHANNEL",
+                            },
+                        ]);
+                    } catch (err) {
+                        interaction.reply({
+                            embeds: [
+                                {
+                                    title: `I can't change permissions in #${chan.name}`,
+                                    description: `\`\`\`${err}\`\`\``,
+                                    footer: {
+                                        text: "I require the Manage Permissions role, and for my highest role to be above the verification role",
+                                    },
+                                },
+                            ],
+                        });
+                        return;
+                    }
+
+                    try {
+                        chan.send({
+                            embeds: [
+                                {
+                                    description:
+                                        "To verify, click this and press enter: </verify:1042262928969170944>",
+                                },
+                            ],
+                        });
+                    } catch (err) {
+                        interaction.reply({
+                            embeds: [
+                                {
+                                    title: `I do not have access to #${chan.name}`,
+                                    description: `\`\`\`${err}\`\`\``,
+                                    footer: {
+                                        text: "Role permissions were successfully changed.",
+                                    },
+                                },
+                            ],
+                        });
+                        return;
+                    }
+
+                    interaction.reply({
+                        embeds: [
+                            {
+                                description: `Verify has been set to \`${modify}d\` \nVerify Channel: <#${chan.id}> \nVerify Role: <@&${role.id}>`,
+                                footer: {
+                                    text: `Moderator: ${moderator}`,
+                                },
+                                color: "GREEN",
+                            },
+                        ],
+                    });
+                } else if (value === false) {
+                    await db.updateOne(
+                        {
+                            "guild.id": interaction.guild.id,
+                        },
+                        {
+                            $set: {
+                                "guild.config.verify.status": value,
+                                "guild.config.verify.channel": null,
+                                "guild.config.verify.role": null,
+                            },
+                        }
+                    );
+                    interaction.reply({
+                        embeds: [
+                            {
+                                description: `Verify has been set to \`${modify}d\``,
+                                footer: {
+                                    text: `Moderator: ${moderator}`,
+                                },
+                                color: "GREEN",
+                            },
+                        ],
+                    });
+                }
+            }
+        } else if (chosen === "logging") {
+            if (value === false) {
+                if (!found) {
+                    await db.insertOne({
+                        guild: {
+                            id: interaction.guild.id,
+                            infractions: {},
+                            config: {
+                                automod: {
+                                    status: false,
+                                    warn: false,
+                                },
+                                verify: {
+                                    status: false,
+                                    channel: null,
+                                    role: null,
+                                },
+                                logging: {
+                                    status: value,
+                                    channel: null,
+                                    level: null,
+                                },
+                            },
+                        },
+                    });
+                    interaction.reply({
+                        embeds: [
+                            {
+                                description: `Logging has been set to \`${modify}d\``,
+                                footer: {
+                                    text: `Moderator: ${moderator}`,
+                                },
+                                color: "GREEN",
+                            },
+                        ],
+                    });
+                } else {
+                    await db.updateOne(
+                        {
+                            "guild.id": interaction.guild.id,
+                        },
+                        {
+                            $set: {
+                                "guild.config.logging.status": value,
+                                "guild.config.logging.channel": null,
+                                "guild.config.logging.level": null,
+                            },
+                        }
+                    );
+                    interaction.reply({
+                        embeds: [
+                            {
+                                description: `Logging has been set to \`${modify}d\``,
+                                footer: {
+                                    text: `Moderator: ${moderator}`,
+                                },
+                                color: "GREEN",
+                            },
+                        ],
+                    });
+                }
+            } else if (value === true) {
+                if (!found) {
+                    await db.insertOne({
+                        guild: {
+                            id: interaction.guild.id,
+                            infractions: {},
+                            config: {
+                                automod: {
+                                    status: false,
+                                    warn: false,
+                                },
+                                verify: {
+                                    status: false,
+                                    channel: null,
+                                    role: null,
+                                },
+                                logging: {
+                                    status: value,
+                                    channel: chan.id,
+                                    level: level,
+                                },
+                            },
+                        },
+                    });
+                    interaction.reply({
+                        embeds: [
+                            {
+                                description: `Logging has been set to \`${modify}d\` \nLogging Channel: <#${chan.id}> \nLogging level: ${level}`,
+                                footer: {
+                                    text: `Moderator: ${moderator}`,
+                                },
+                                color: "GREEN",
+                            },
+                        ],
+                    });
+                } else {
+                    await db.updateOne(
+                        {
+                            "guild.id": interaction.guild.id,
+                        },
+                        {
+                            $set: {
+                                "guild.config.logging.status": value,
+                                "guild.config.logging.channel": chan.id,
+                                "guild.config.logging.level": level,
+                            },
+                        }
+                    );
+                    interaction.reply({
+                        embeds: [
+                            {
+                                description: `Logging has been set to \`${modify}d\` \nLogging Channel: <#${chan.id}> \nLogging level: ${level}`,
+                                footer: {
+                                    text: `Moderator: ${moderator}`,
+                                },
+                                color: "GREEN",
+                            },
+                        ],
+                    });
+                }
+            } else {
+                console.log("what the fuck");
+            }
+        }
+    },
+};
---
+++ new Config.js
@@ -0,0 +1,265 @@
+/** @format */
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageActionRow, MessageButton } = require("discord.js");
+const chalk = require("chalk");
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("test")
+        .setDescription("Change bot configurations."),
+
+    async execute(interaction, client) {
+        const db = await client.db.collection("Infractions");
+        console.log(
+            `${chalk.greenBright(
+                "[EVENT ACKNOWLEDGED]"
+            )} interactionCreate with command test`
+        );
+
+        const moderator = interaction.user.tag;
+        const found = await db.findOne({ "guild.id": interaction.guild.id });
+
+        if (
+            !interaction.member.permissions.has("ADMINISTRATOR") ||
+            !interaction.guild.members.me.permissions.has("ADMINISTRATOR")
+        ) {
+            interaction.reply({
+                embeds: [
+                    {
+                        description: "You are not a moderator!",
+                        footer: {
+                            text: "This requires the Administrator permission.",
+                        },
+                    },
+                ],
+            });
+            return;
+        }
+
+        if (!found) {
+            await db.insertOne({
+                guild: {
+                    id: interaction.guild.id,
+                    infractions: {},
+                    config: {
+                        automod: {
+                            status: false,
+                            warn: null,
+                        },
+                        verify: {
+                            status: false,
+                            channel: null,
+                            role: null,
+                        },
+                        logging: {
+                            status: false,
+                            channel: null,
+                            level: null,
+                        },
+                    },
+                },
+            });
+        }
+        const buttons = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId("verify")
+                    .setLabel("Verification")
+                    .setStyle("SUCCESS")
+            )
+            .addComponents(
+                new MessageButton()
+                    .setCustomId("automod")
+                    .setLabel("Automod")
+                    .setStyle("SUCCESS")
+            )
+            .addComponents(
+                new MessageButton()
+                    .setCustomId("logging")
+                    .setLabel("Logging")
+                    .setStyle("SUCCESS")
+            )
+            .addComponents(
+                new MessageButton()
+                    .setCustomId("cancel")
+                    .setLabel("Cancel")
+                    .setStyle("DANGER")
+            );
+
+        interaction.reply({
+            embeds: [
+                {
+                    fields: [
+                        {
+                            name: "Verification",
+                            value: "Select Verification to enable/modify server verification on member join.",
+                            inline: true,
+                        },
+                        {
+                            name: "Automod",
+                            value: "Select Automod to enable/modify this server's automatic moderation strictness",
+                            inline: true,
+                        },
+                        {
+                            name: "Logging",
+                            value: "Select logging to enable/modify this server's logging strictness.",
+                            inline: true,
+                        },
+                    ],
+                    footer: {
+                        text: "Select cancel to end this interaction.",
+                    },
+                },
+            ],
+            components: [buttons],
+            ephemeral: true,
+        });
+
+        const response1 = await interaction.channel
+            .awaitMessageComponent({
+                filter: (i) => {
+                    return (
+                        i.customId === "verify" ||
+                        i.customId === "automod" ||
+                        i.customId === "logging" ||
+                        i.customId === "cancel"
+                    );
+                },
+                time: 30_000,
+            })
+            .catch(() => null);
+
+        if (response1 === null) {
+            await interaction
+                .editReply({
+                    embeds: [
+                        {
+                            title: "This command has expired.",
+                        },
+                    ],
+                    components: [],
+                })
+                .catch();
+            return;
+        }
+
+        const automod = await db.findOne({
+            "guild.id": interaction.guild.id,
+            [`guild.config.automod`]: {
+                $exists: true,
+            },
+        });
+        const logging = await db.findOne({
+            "guild.id": interaction.guild.id,
+            [`guild.config.logging`]: {
+                $exists: true,
+            },
+        });
+
+        if (response1.customId === "verify") {
+            interaction.editReply({
+                embeds: [
+                    {
+                        title: "You selected Verify.",
+                    },
+                ],
+                components: [],
+            });
+        }
+
+        if (response1.customId === "automod") {
+            let statusButton;
+            if (automod.guild.config.automod.status === true) {
+                statusButton = "Enable";
+            } else {
+                statusButton = "Disable";
+            }
+
+            let fresh;
+            if (automod.guild.config.automod.status === true) {
+                fresh = "Disable";
+            } else {
+                fresh = "Enable";
+            }
+
+            // FIRST SET OF BUTTONS AND HANDLING
+            const firstSet = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setCustomId("Modify")
+                        .setLabel("Modify")
+                        .setStyle("SUCCESS")
+                )
+                .addComponents(
+                    new MessageButton()
+                        .setCustomId(fresh)
+                        .setLabel(fresh)
+                        .setStyle("DANGER")
+                );
+            interaction.editReply({
+                embeds: [
+                    {
+                        title: `Automod is currently set to ${statusButton}d.`,
+                    },
+                ],
+                components: [firstSet],
+            });
+
+            const freshResponse = await interaction.channel
+                .awaitMessageComponent({
+                    filter: (i) => {
+                        return i.customId === "Modify" || i.customId === fresh;
+                    },
+                    time: 30_000,
+                })
+                .catch(() => null);
+
+            if (freshResponse === null) {
+                await interaction
+                    .editReply({
+                        embeds: [
+                            {
+                                title: "This command has expired.",
+                            },
+                        ],
+                        components: [],
+                    })
+                    .catch();
+                return;
+            }
+            if (freshResponse.customId === fresh) {
+                // DB SET TO FRESH
+                interaction.editReply({
+                    embeds: [
+                        {
+                            title: "wtf",
+                        },
+                    ],
+                    components: [],
+                });
+            }
+        }
+
+        if (response1.customId === "logging") {
+            interaction.editReply({
+                embeds: [
+                    {
+                        title: "You selected Logging.",
+                    },
+                ],
+                components: [],
+            });
+        }
+        if (response1.customId === "cancel") {
+            interaction.editReply({
+                embeds: [
+                    {
+                        title: "You cancelled this interaction.",
+                    },
+                ],
+                components: [],
+            });
+            return;
+        }
+    },
+};
